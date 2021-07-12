c9a1b7b0d124168bfa6da0e6438496e33563057f
Ahmet SOYLU <86555297+ahmet3202@users.noreply.github.com>
2021-07-12 14:13:18 +0300

    Update struct_örnek.c

--- struct_örnek.c
+++ struct_örnek.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	struct sinemaFilm
+	struct sinemaFilm  // sınemefiladında bir yapı oluşturup içine char ve int tipinde değişken atadım.
 	{
 			char filmadi[50];
 			char filmyonetmen[50];
@@ -11,7 +11,7 @@
 		int imdb_puan;
 		};
 
-struct arabakayit{
+struct arabakayit{   //  arabakayıt tipinde stsruct oluşturdum yukarıdaki gibi bilgileri tek bir yapı içibde topladık.
 	
 	char araba_ad[20];
 	int araba_yil;
@@ -23,20 +23,20 @@ struct arabakayit{
 
 int main(int argc, char *argv[]) {
 	
-		struct sinemaFilm aksiyon;
-		strcpy(aksiyon.filmadi,"EXTRACTION");
+		struct sinemaFilm aksiyon;                // aksiyon değerini atayıp oradan işlem yapalım
+		strcpy(aksiyon.filmadi,"EXTRACTION");    // strcpy ile değerleri kendim elle girdim siz klavyeden de yapabilirsiniz.
 		strcpy(aksiyon.filmyonetmen,"Ahmet SOYLU");
 		aksiyon.yapimtarih="2018";
 		aksiyon.imdb_puan="7.8";
 	printf("Film adi: %s\n  yonetmeni: %s\n film yapim tarihi : %s\n film imdb puani :%s\n",aksiyon.filmadi,aksiyon.filmyonetmen,aksiyon.yapimtarih,aksiyon.imdb_puan);
-	printf("\n");
-struct arabakayit marka;
-strcpy(marka.araba_ad,"OPEL");
-strcpy(marka.araba_renk,"siyah");
-marka.araba_yil="1995";
+	printf("\n");  // değerleri ekranda görüntülemek için structları kullanarak kodu yazdım.
+               struct arabakayit marka;
+	       strcpy(marka.araba_ad,"OPEL");
+	       strcpy(marka.araba_renk,"siyah");
+	       marka.araba_yil="1995";
 
 
-printf(" Araba markasi : %s\n araba rengi %s\n araba cikis yili %s\n",marka.araba_ad,marka.araba_renk,marka.araba_yil);
+      printf(" Araba markasi : %s\n araba rengi %s\n araba cikis yili %s\n",marka.araba_ad,marka.araba_renk,marka.araba_yil);
 
 
 
