5c5a6cf8f8135f1df97fc9e18459ad62f8edd195
Ahmet SOYLU <86555297+ahmet3202@users.noreply.github.com>
2021-07-13 15:45:07 +0300

    Add files via upload

---
+++ pointer_dolaylı_deger_degisim.c
@@ -0,0 +1,19 @@
+		#include<stdio.h>
+		#include<stdlib.h>
+		
+		
+		int main()
+		{
+		int sayideger=250;         // ilk deger bu ekranda 250 görünür
+		printf("%d ilk deger\n",sayideger);
+		int *ptrdeger;   // pointer ile bellek adresine ulaşıp değeri değiştireğiz.
+		ptrdeger=&sayideger;   // sayideger değişkeninin adresini ptrdeger adlı pointer tutar adresi ise & işareti göstermiş olur.
+		*ptrdeger=500;    // ikinci kısımda degerini 500 yapalım
+		int sayideger2=*ptrdeger;
+		printf("%d ikinci deger\n",sayideger2);// 500 görünür
+		*ptrdeger=900;
+		printf("%d Son deger\n",*ptrdeger);  // 900 görünür.
+	system("pause");
+		return 0;
+		
+}
